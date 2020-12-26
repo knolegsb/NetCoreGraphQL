4ed8fd3c6cade02a24e53619a234b9b492f7cee8
seancbs <sean.y@centurybizsolutions.com>
2020-12-26 12:22:12 -0800

    first commit

---
+++ NetCoreGraphQL/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using NetCoreGraphQL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreGraphQL
+{
+    public interface IProduct
+    {
+        List<Product> GetAllProducts();
+        Product AddProduct(Product product);
+        Product UpdateProduct(int id, Product product);
+        void DeleteProduct(int id);
+        Product GetProductById(int id);
+    }
+}
